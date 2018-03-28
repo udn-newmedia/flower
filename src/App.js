e2d78acf6d522e69eb447d99f0005100a5da3671
Waiting7777 <waiting0819@msn.com>
2018-03-28 09:58:17 +0800

    change to one canvas

--- src/App.js
+++ src/App.js
@@ -15,85 +15,97 @@ class App extends Component {
     this.nextPage = this.nextPage.bind(this)
     this.mouseDownHandle = this.mouseDownHandle.bind(this)
     this.mouseUpHandle = this.mouseUpHandle.bind(this)
+    this.videoCanplay = this.videoCanplay.bind(this)
 
-    this.state = {stage: 0, left: false, right: false, buttonl: false, buttonr: false, canvas1: 0, canvas2: 100, video: null, video2: null}
+    this.state = {stage: 0, left: false, right: false, buttonl: false, buttonr: false, canvas1: 0, canvasAnimate: true}
+  }
+
+  videoCanplay(){
+    var video = document.getElementById('video')
+    if(video.paused === true){
+      video.play()
+    }
   }
 
   videoEnded(){
     
-    var video, video2
+    var video = document.getElementById('video')
 
     if(this.state.stage === 0) {  
-      video = document.getElementById('video-2')
-      video.play()
-      video2 = document.getElementById('video-7')
-      this.setState({stage: 1, video: video, buttonr: true, video2: video2})
+      video.src = './video/q1.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({stage: 1, buttonr: true})
     }
     else if(this.state.stage === 1) {
       if(this.state.left === true) {
-        video = document.getElementById('video-4')
-        video.currentTime = 0
-        video.play()
-        this.setState({video: video})
+        video.src = './video/q1-left.mp4'
+        video.loop = true
+        // video.play()
       }
       else{
-        video = document.getElementById('video-6')
-        video.currentTime = 0
-        video.play()
-        this.setState({video: video})
+        video.src = './video/q1-right.mp4'
+        video.loop = true
+        // video.play()
       }
     }
     else if(this.state.stage === 2) {
       if(this.state.left === true) {
-        video = document.getElementById('video-9')
-        video.currentTime = 0
-        video.play()
-        this.setState({video2: video})
+        video.src = './video/q2-left.mp4'
+        video.loop = true
+        // video.play()
       }
       else{
-        video = document.getElementById('video-11')
-        video.currentTime = 0
-        video.play()
-        this.setState({video2: video})
+        video.src = './video/q2-right.mp4'
+        video.loop = true
+        // video.play()
       }
     }
   }
 
   canvasTouchStart(e) {
     console.log(123)
+    if(window.innerWidth > 768){
+      return
+    }
+
     if(this.state.stage < 1){
       return
     }
     console.log(456)
    
-    var video
+    var video = document.getElementById('video')
 
     if(e.touches[0].pageX < window.innerWidth / 2){
 
       if(this.state.stage === 1){
-        video = document.getElementById('video-3')
-        video.play()
-        this.setState({left: true, video: video})
+        video.src = './video/q1-left-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({left: true})
       }
       
       else if(this.state.stage === 2) {
-        video = document.getElementById('video-8')
-        video.play()
-        this.setState({left: true, video2: video})
+        video.src = './video/q2-left-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({left: true})
       }
       
       console.log('left')
     }
     else{
       if(this.state.stage === 1){
-        video = document.getElementById('video-5')
-        video.play()
-        this.setState({right: true, video: video})
+        video.src = './video/q1-right-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({right: true})
       }
       else if(this.state.stage === 2) {
-        video = document.getElementById('video-10')
-        video.play()
-        this.setState({right: true, video2: video})
+        video.src = './video/q2-right-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({right: true})
       }
       console.log('right')
     }
@@ -106,118 +118,126 @@ class App extends Component {
   }
 
   canvasTouchEnd(e){
-    var video, temp, i
+    var video = document.getElementById('video')
+    if(window.innerWidth > 768){
+      return
+    }
     if(this.state.stage === 1) {
-      video = document.getElementById('video-2')
-      for(i = 3; i <=6;i++){
-        temp = document.getElementById('video-' + i)
-        temp.pause()
-      }
-      this.setState({left: false, right: false, video: video})
+      video.src = './video/q1.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({left: false, right: false})
     }
     else if(this.state.stage === 2) {
-      video = document.getElementById('video-7')
-      for(i = 8; i <= 11;i++){
-        temp = document.getElementById('video-' + i)
-        temp.pause()
-      }
-      this.setState({left: false, right: false, video2: video})
+      video.src = './video/q2.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({left: false, right: false})
     }
   }
 
   mouseDownHandle(e){
     console.log(123)
-    console.log(123)
+    if(window.innerWidth <= 768){
+      return
+    }
     if(this.state.stage < 1){
       return
     }
     console.log(456)
    
-    var video
+    var video = document.getElementById('video')
 
     if(e.pageX < window.innerWidth / 2){
 
       if(this.state.stage === 1){
-        video = document.getElementById('video-3')
-        video.play()
-        this.setState({left: true, video: video})
+        video.src = './video/q1-left-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({left: true})
       }
       
       else if(this.state.stage === 2) {
-        video = document.getElementById('video-8')
-        video.play()
-        this.setState({left: true, video2: video})
+        video.src = './video/q2-left-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({left: true})
       }
       
       console.log('left')
     }
     else{
       if(this.state.stage === 1){
-        video = document.getElementById('video-5')
-        video.play()
-        this.setState({right: true, video: video})
+        video.src = './video/q1-right-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({right: true})
       }
       else if(this.state.stage === 2) {
-        video = document.getElementById('video-10')
-        video.play()
-        this.setState({right: true, video2: video})
+        video.src = './video/q2-right-turn.mp4'
+        video.loop = false
+        // video.play()
+        this.setState({right: true})
       }
       console.log('right')
     }
   }
 
   mouseUpHandle(e){
-    var video, temp, i
+    var video = document.getElementById('video')
+    if(window.innerWidth <= 768){
+      return
+    }
     if(this.state.stage === 1) {
-      video = document.getElementById('video-2')
-      for(i = 3; i <=6;i++){
-        temp = document.getElementById('video-' + i)
-        temp.pause()
-      }
-      this.setState({left: false, right: false, video: video})
+      video.src = './video/q1.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({left: false, right: false})
     }
     else if(this.state.stage === 2) {
-      video = document.getElementById('video-7')
-      for(i = 8; i <= 11;i++){
-        temp = document.getElementById('video-' + i)
-        temp.pause()
-      }
-      this.setState({left: false, right: false, video2: video})
+      video.src = './video/q2.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({left: false, right: false})
     }
   }
 
   prevPage(){
     console.log(4)
+    var video = document.getElementById('video')
     if(this.state.stage === 2){
-      var video = document.getElementById('video-2')
-      video.currentTime = 0
-      video.play()
-      this.setState({stage: 1, buttonl: false, buttonr: true, canvas1: 0, canvas2: 100})
+      video.src = './video/q1.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({stage: 1, buttonl: false, buttonr: true, canvas1: 100, canvasAnimate: true})
+      setTimeout(() => {
+        this.setState({canvas1: 0, canvasAnimate: false})
+      }, 700)
     }
   }
 
   nextPage() {
     console.log(5)
+    var video = document.getElementById('video')
     if(this.state.stage === 1){
-      var video = document.getElementById('video-7')
-      video.currentTime = 0
-      video.play()
-      this.setState({stage: 2, buttonl: true, buttonr: false, canvas1: -100, canvas2: 0})
+      video.src = './video/q2.mp4'
+      video.loop = true
+      // video.play()
+      this.setState({stage: 2, buttonl: true, buttonr: false, canvas1: -100, canvasAnimate: true})
+      setTimeout(() => {
+        this.setState({canvas1: 0, canvasAnimate: false})
+      }, 700)
     }
   }
 
   componentDidMount() {
+    
     var canvas = document.getElementById('canvas')
     var ctx = canvas.getContext('2d')
     var video = document.getElementById('video')
-    var canvas2 = document.getElementById('canvas2')
-    var ctx2 = canvas2.getContext('2d')
-    var video2 = document.getElementById('video-7')
-    this.setState({video : video, video2: video2 })
-    
     setInterval(() => {
-      ctx.drawImage(this.state.video, 0, 0, 1920, 1080)
-      ctx2.drawImage(this.state.video2, 0, 0, 1920, 1080)
+      
+      ctx.drawImage(video, 0, 0, 1920, 1080)
     }, 33)
     
   }
@@ -234,12 +254,12 @@ class App extends Component {
       'hidden': !this.state.buttonr
     })
 
+    var canvasClass = classNames({
+      'animate': this.state.canvasAnimate
+    })
+
     var canvasStyle1 = {
       transform: 'translate(' + this.state.canvas1 + '%, 0)'
-    };
-
-    var canvasStyle2 = {
-      transform: 'translate(' + this.state.canvas2 + '%, 0)'
     };
 
     return (
@@ -268,20 +288,9 @@ class App extends Component {
           <img className="hidden-md hidden-lg" src="./image/buttonr.png" alt="buttonr" />
           <img className="hidden-xs hidden-sm" src="./image/web/buttonr.png" alt="buttonr" />
         </div>
-        <canvas id="canvas" width="1920" height="1080" style={canvasStyle1} onTouchStart={this.canvasTouchStart} onTouchEnd={this.canvasTouchEnd} onMouseUp={this.mouseUpHandle} onMouseDown={this.mouseDownHandle} />
-        <canvas id="canvas2" width="1920" height="1080" style={canvasStyle2} onTouchStart={this.canvasTouchStart} onTouchEnd={this.canvasTouchEnd} onMouseUp={this.mouseUpHandle} onMouseDown={this.mouseDownHandle} />
+        <canvas id="canvas" className={canvasClass} width="1920" height="1080" style={canvasStyle1} onTouchStart={this.canvasTouchStart} onTouchEnd={this.canvasTouchEnd} onMouseUp={this.mouseUpHandle} onMouseDown={this.mouseDownHandle} />
         <div id="video-contain">
-          <video id="video" src="./video/intro.mp4" onEnded={this.videoEnded} playsInline />
-          <video id="video-2" src="./video/q1.mp4" onEnded={this.videoEnded} playsInline loop muted/>
-          <video id="video-3" src="./video/q1-left-turn.mp4" onEnded={this.videoEnded} playsInline muted/>
-          <video id="video-4" src="./video/q1-left.mp4" onEnded={this.videoEnded} playsInline loop muted/>
-          <video id="video-5" src="./video/q1-right-turn.mp4" onEnded={this.videoEnded} playsInline muted/>
-          <video id="video-6" src="./video/q1-right.mp4" onEnded={this.videoEnded} playsInline loop muted/>
-          <video id="video-7" src="./video/q2.mp4" onEnded={this.videoEnded} playsInline loop muted/>
-          <video id="video-8" src="./video/q2-left-turn.mp4" onEnded={this.videoEnded} playsInline muted/>
-          <video id="video-9" src="./video/q2-left.mp4" onEnded={this.videoEnded} playsInline loop muted/>
-          <video id="video-10" src="./video/q2-right-turn.mp4" onEnded={this.videoEnded} playsInline muted/>
-          <video id="video-11" src="./video/q2-right.mp4" onEnded={this.videoEnded} playsInline loop muted/>
+          <video id="video" src="./video/intro.mp4" onEnded={this.videoEnded} onCanPlay={this.videoCanplay} playsInline />
         </div>
       </div>
     );
